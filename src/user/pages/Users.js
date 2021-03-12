6fc54b2f1e1f373ea91192752b04bdc2458b131d
ravi.kant <ravi.kant@magicedtech.com>
2021-03-12 20:58:09 +0530

    card and avatar component added

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = (props) => {
-  return <div className="users">UERS WORKS</div>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Ravi",
+      image:
+        "https://image.shutterstock.com/shutterstock/photos/1714550221/display_1500/stock-photo-muscular-bodybuilder-with-jar-of-protein-on-a-black-background-sports-nutrition-bodybuilding-1714550221.jpg",
+      places: 10,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
